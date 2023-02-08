fef76d0ba02903c9ab38b1f3b9cef219e7433fc7
Peter Szabo <szpeter0114@gmail.com>
2023-02-08 16:13:39 +0900

    add files

---
+++ app.js
@@ -0,0 +1,71 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            title: 'Predator',
+            cover: 'https://www.mobygames.com/images/covers/l/220956-predator-zx-spectrum-front-cover.jpg',
+            plot: 'A team of commandos on a mission in a Central American jungle find themselves hunted by an extraterrestrial warrior.',
+            year: 1987,
+            showCover: false,
+        }
+    },
+    methods: {
+        toggleDesc() {
+            this.showCover = !this.showCover;
+        }
+    }
+})
+app.mount('#app')
+
+const app2 = Vue.createApp({
+    data() {
+        return {
+            title: 'Terminator',
+            cover : 'https://upload.wikimedia.org/wikipedia/en/8/85/Terminator2poster.jpg',
+            plot: 'A cyborg, identical to the one who failed to kill Sarah Connor, must now protect her 10-year old son John from an even more advanced and powerful cyborg.',
+            year: 1991,
+            showCover: false,
+        }
+    },
+    methods: {
+        toggleDesc() {
+            this.showCover = !this.showCover;
+        }
+    }
+})
+app2.mount('#app2')
+
+const app3 = Vue.createApp({
+    data() {
+        return {
+            title: 'Conan the Barbarian',
+            cover : 'https://m.media-amazon.com/images/M/MV5BMWYwZGU1N2UtNTU5My00ZTVkLThjZDktZTQzZmEyMzZjMTdmXkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_.jpg',
+            plot: 'A young boy, Conan, becomes a slave after his parents are killed and tribe destroyed by a savage warlord and sorcerer, Thulsa Doom. When he grows up he becomes a fearless, invincible fighter. Set free, he plots revenge against Thulsa Doom.',
+            year: 1982,
+            showCover: false,
+        }
+    },
+    methods: {
+        toggleDesc() {
+            this.showCover = !this.showCover;
+        }
+    }
+})
+app3.mount('#app3')
+
+const app4 = Vue.createApp({
+    data() {
+        return {
+            title: 'Total Recall',
+            cover : 'https://m.media-amazon.com/images/M/MV5BYzU1YmJjMGEtMjY4Yy00MTFlLWE3NTUtNzI3YjkwZTMxZjZmXkEyXkFqcGdeQXVyNDc2NjEyMw@@._V1_FMjpg_UX1000_.jpg',
+            plot: 'When a man goes in to have virtual vacation memories of the planet Mars implanted in his mind, an unexpected and harrowing series of events forces him to go to the planet for real - or is he?',
+            year: 1990,
+            showCover: false,
+        }
+    },
+    methods: {
+        toggleDesc() {
+            this.showCover = !this.showCover;
+        }
+    }
+})
+app4.mount('#app4')
